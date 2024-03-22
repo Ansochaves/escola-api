ef6fdba5cc39090c38c0de3ea8e42cb677d94cd9
Ansochaves <anderson@gwsistemas.com.br>
2024-03-22 20:54:02 -0300

    Interface repository montada

--- src/main/java/com/escola/api/repository/TurmaRepository.java
+++ src/main/java/com/escola/api/repository/TurmaRepository.java
@@ -1,5 +1,9 @@
 package com.escola.api.repository;
 
-public interface TurmaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.escola.api.model.Turma;
+
+public interface TurmaRepository extends JpaRepository<Turma, Long>{
 
 }
