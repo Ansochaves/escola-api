7923ad62cbdebc3b32fc11a0a84d396d8709193a
Ansochaves <anderson@gwsistemas.com.br>
2024-03-22 21:18:24 -0300

    Metodos de salvar consultar e buscar por id criados no controller

--- src/main/java/com/escola/api/controller/TurmaController.java
+++ src/main/java/com/escola/api/controller/TurmaController.java
@@ -1,5 +1,47 @@
 package com.escola.api.controller;
 
-public class TurmaController {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.escola.api.model.Turma;
+import com.escola.api.repository.TurmaRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/")
+public class TurmaController {
+	
+	@Autowired
+	private TurmaRepository turmaRepository;
+	
+	@GetMapping("/turmas")
+	public ResponseEntity<List<Turma>> getAllTurmas(){
+		return ResponseEntity.status(HttpStatus.OK).body(turmaRepository.findAll());
+	}
+	
+	@GetMapping("/turmas/{id}")
+	public ResponseEntity<Optional<Turma>> getTurmaById(@PathVariable Long id){
+		return ResponseEntity.status(HttpStatus.OK).body(turmaRepository.findById(id));
+	}
+	
+	@PostMapping("/turmas")
+	public ResponseEntity<Turma> cadastrarTurmas(@RequestBody Turma turma){
+		return ResponseEntity.status(HttpStatus.CREATED).body(turmaRepository.save(turma));
+		
+	}
+	
+	
+	
 }
